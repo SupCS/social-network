7ac06bea7dbfc75ec8f06f4830eacf7584405217
Dmytro Asparian <supezbiz@gmail.com>
2023-11-05 13:36:56 +0200

    Beginning login functionality

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import classes from './Header.module.css'
+import React from "react";
+import classes from "./Header.module.css";
+import { NavLink } from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
   return (
-  <header className={classes.header}>
-    <img
-      src="https://upload.wikimedia.org/wikipedia/commons/d/db/Zeronet_logo.png"
-      alt="header logo"
-    ></img>
-  </header>
-  )
-}
+    <header className={classes.header}>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/d/db/Zeronet_logo.png"
+        alt="header logo"
+      ></img>
+      <div className={classes.loginBlock}>
+        {props.isAuth ? props.login : <NavLink to={"/login"}>Login</NavLink>}
+      </div>
+    </header>
+  );
+};
 
 export default Header;
