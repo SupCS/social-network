411ee95ade07208d3cb11d4381a45addc27edb12
Dmytro Asparian <supezbiz@gmail.com>
2023-12-24 10:32:25 +0200

    Messages fix

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
 import AddMessageForm from "./AddMessageForm/AddMessageForm.jsx";
@@ -18,8 +18,17 @@ const Dialogs = ({
   isAuth,
   currentUserId,
 }) => {
+  const messagesEndRef = useRef(null);
   const { userId } = useParams(); // Получаем userId из URL
   const dispatch = useDispatch();
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    scrollToBottom(); // Скролл при монтировании компонента
+  }, [messagesPage.messages]);
 
   useEffect(() => {
     getDialogs();
@@ -62,15 +71,17 @@ const Dialogs = ({
   let dialogsElements = messagesPage.dialogs.map((d) => (
     <DialogItem name={d.name} key={d.id} id={d.id} />
   ));
-  let messagesElements = messagesPage.messages.map((m) => (
-    <Message
-      message={m.text}
-      key={m._id}
-      senderId={m.senderId}
-      senderName={m.senderName}
-      currentUserId={currentUserId}
-    />
-  ));
+  let messagesElements = messagesPage.messages.map((m) => {
+    return (
+      <Message
+        message={m.text}
+        key={m._id}
+        senderId={m.senderId}
+        senderName={m.senderName}
+        currentUserId={currentUserId}
+      />
+    );
+  });
 
   if (!isAuth) return <Navigate to={"/login"} />;
 
@@ -79,6 +90,9 @@ const Dialogs = ({
       <div className={classes.dialogsItems}>{dialogsElements}</div>
       <div className={classes.messages}>
         {userId ? messagesElements : null}
+        <div ref={messagesEndRef}></div>
+      </div>
+      <div className={classes.messageInput}>
         {userId && <AddMessageForm onSubmit={addNewMessage} />}
       </div>
     </div>
--- src/components/Dialogs/Message/Message.jsx
+++ src/components/Dialogs/Message/Message.jsx
@@ -3,13 +3,19 @@ import classes from "./../Dialogs.module.css";
 
 const Message = ({ message, senderId, senderName, currentUserId }) => {
   console.log("Current" + currentUserId);
-  console.log("Sender" + senderId);
+  console.log(`Sender: ${senderId}`);
   console.log("Message component props:", { message, senderId, currentUserId });
   const isCurrentUser = senderId === currentUserId;
+  console.log(`Is current in mes: ${isCurrentUser}`);
   const author = isCurrentUser ? "You:" : `${senderName || "Unknown user"}:`;
+  // Инлайн-стили для расположения сообщения
+  console.log(isCurrentUser);
+  const messageStyle = isCurrentUser
+    ? { justifyContent: "flex-end", textAlign: "right" }
+    : { justifyContent: "flex-start", textAlign: "left" };
 
   return (
-    <div className={classes.messageItem}>
+    <div className={classes.messageItem} style={messageStyle}>
       <div className={classes.messageAuthor}>{author}</div>
       <div className={classes.message}>{message}</div>
     </div>
